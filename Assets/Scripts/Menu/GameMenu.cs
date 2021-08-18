2f6fe6ceba81529826e12ed6c2d7ef2f8541275f
Adam <weterby2@gmail.com>
2021-08-18 18:02:04 +0200

    Fix GameScene bug
    Returning to main menu and creating a new game no longer cause freeze

--- Assets/Scripts/Menu/GameMenu.cs
+++ Assets/Scripts/Menu/GameMenu.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour
+public class GameMenu : MonoBehaviour
 {
     private bool isScenePaused = false;
     [SerializeField]
@@ -16,14 +16,9 @@ public class MainMenu : MonoBehaviour
         SceneManager.LoadScene("GameScene");
     }
 
-    // Update is called once per frame
-     public void QuitButton()
-    {
-        Application.Quit();
-    }
-
     public void BackToMainMenu()
     {
+        Time.timeScale = 1f;
         SceneManager.LoadScene("MainMenu");
     }
 
